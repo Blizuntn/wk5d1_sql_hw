0a405368825ff8524b322598cd0267be12d0e337
Michael Bunton <mbnt84@gmail.com>
2022-07-06 08:40:53 -0500

    Im missing a few questions but i will finish them and turn in a completed copy

---
+++ main.py
@@ -0,0 +1,29 @@
+#1 How many actors are there with the last name "Wahlberg"?
+2
+
+#2 How many payments were made between $3.99 and $5.99?
+5607
+
+#3 What film does the store have the most of?(search inventory)
+
+
+#4 How many customers have the last name William?
+
+0
+#5 What store employee (get the id) sold the most rentals?
+1
+
+#6 How many different district names are there?
+
+
+#7 What film has the most actors in it?
+
+
+#8 From store_id 1 how many customers have a last name ending with "es"?
+
+
+#9 How many payment amounts (4.99, 5.99, etc.) had a number of rentals above 250 customers with ids between380 and 430?
+# (use group by and having > 250)
+
+
+#10 Within the film table how many rating categories are there? And what rating has the most movies total?
